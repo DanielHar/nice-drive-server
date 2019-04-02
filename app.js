d4396a1d82d9723f6cf982c7ddf8986eda3e403c
Daniel Harit <daniel.harit@gmail.com>
2019-04-02 19:04:05 +0300

    fix space

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 const config = require('./config/config');
 const path = require('path');
 
-app.listen(process.env.PORT ||config.port);
+app.listen(process.env.PORT || config.port);
 console.log(`Node server is running in port ${config.port}`);
 app.use(express.static(path.join(__dirname, 'public')))
 
